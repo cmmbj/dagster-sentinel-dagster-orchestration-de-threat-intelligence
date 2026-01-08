7264160849d290627e061606e8bad05237f92df5
cmmbj <facebaketimeqlf1@gmail.com>
2026-01-08 15:56:30 +0100

    Modificitation de definitions.py

--- my_cyber_project/definitions.py
+++ my_cyber_project/definitions.py
@@ -1,5 +1,5 @@
 import os
-from dagster import Definitions, load_assets_from_modules, define_asset_job
+from dagster import Definitions, load_assets_from_modules, define_asset_job, ScheduleDefinition # 1. Ajout de l'import
 from dotenv import load_dotenv
 
 # Imports absolus
@@ -9,13 +9,19 @@ load_dotenv()
 
 all_assets = load_assets_from_modules([assets])
 
-# --- NOUVEAU : On définit le job que le sensor va appeler ---
-# Ce job va simplement matérialiser les assets de sécurité
+# On définit le job (tu l'as déjà fait)
 cyber_job = define_asset_job(name="alert_job", selection="*") 
+
+# 2. NOUVEAU : On définit le planning (Schedule)
+cyber_schedule = ScheduleDefinition(
+    name="cyber_schedule",
+    job=cyber_job,
+    cron_schedule="*/30 * * * *" # Toutes les 30 minutes
+)
 
 defs = Definitions(
     assets=all_assets,
-    jobs=[cyber_job], # <--- On ajoute le job ici
+    jobs=[cyber_job],
     resources={
         "database": resources.DuckDBResource(
             database_path="data/cyber_data.db"
@@ -25,5 +31,7 @@ defs = Definitions(
             abuseipdb_api_key=os.getenv("ABUSEIPDB_API_KEY", "")
         ),
     },
+    # 3. On ajoute la liste des schedules ici
+    schedules=[cyber_schedule], 
     sensors=[sensors.cve_alert_sensor],
 )
